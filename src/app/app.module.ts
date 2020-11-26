20d53b71c94cd84d04da757d39c0da478b0ae6d9
mauriayllon <mauricio.ad1707@gmail.com>
2020-11-26 08:44:11 -0400

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-
 import { AppComponent } from "./app.component";
+import { Routes, RouterModule } from "@angular/router";
+
+const routes: Routes = [
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./modules/login/login.module").then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
