b56d16f71cff27edee1bd0dd11663c80bf0abfbc
mauriayllon <mauricio.ad1707@gmail.com>
2020-12-01 23:39:17 -0400

    aprobado, rechazado, en espera components

--- src/app/modules/home/modules/historial/historial.module.ts
+++ src/app/modules/home/modules/historial/historial.module.ts
@@ -3,9 +3,17 @@ import { CommonModule } from "@angular/common";
 import { HistorialComponent } from "./historial.component";
 import { HistorialRoutingModule } from "./historial-routing.module";
 import { MatTabsModule } from "@angular/material/tabs";
+import { AprobadoComponent } from "./components/aprobado/aprobado.component";
+import { EsperaComponent } from "./components/espera/espera.component";
+import { RechazadoComponent } from "./components/rechazado/rechazado.component";
 
 @NgModule({
   imports: [CommonModule, HistorialRoutingModule, MatTabsModule],
-  declarations: [HistorialComponent]
+  declarations: [
+    HistorialComponent,
+    AprobadoComponent,
+    EsperaComponent,
+    RechazadoComponent
+  ]
 })
 export class HistorialModule {}
