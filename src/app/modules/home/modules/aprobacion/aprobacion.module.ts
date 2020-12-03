980b51b1b9011126722e8bca267edbd01c4fe5e8
mauriayllon <mauricio.ad1707@gmail.com>
2020-12-02 23:20:02 -0400

    Formulario back up

--- src/app/modules/home/modules/aprobacion/aprobacion.module.ts
+++ src/app/modules/home/modules/aprobacion/aprobacion.module.ts
@@ -2,9 +2,23 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { AprobacionComponent } from "./aprobacion.component";
 import { AprobacionRoutingModule } from "./aprobacion-routing.module";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-  imports: [CommonModule, AprobacionRoutingModule],
+  imports: [
+    CommonModule,
+    AprobacionRoutingModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule
+  ],
   declarations: [AprobacionComponent]
 })
 export class AprobacionModule {}
